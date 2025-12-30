a3e3da9f87e7a6d4d0eb7ad19694ee99e1b2590e
info <info@shiva>
2025-12-30 21:22:11 +0530

    g

---
+++ Sri indu/com/tnsif/Demo/Day5/ThisKey.java
@@ -0,0 +1,11 @@
+package com.tnsif.Demo.Day5;
+
+public class ThisKey {
+	int a=20;
+	public void show() {
+		int a=10;
+		System.out.println("Local variable"+a);
+		System.out.println("Instance variable"+this.a);
+	}
+
+}
