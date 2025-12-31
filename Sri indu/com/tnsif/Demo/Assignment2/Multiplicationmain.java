6dd534804d8578b46b4f361d87713d07e2db47b0
info <info@shiva>
2025-12-31 10:39:58 +0530

    A2

---
+++ Sri indu/com/tnsif/Demo/Assignment2/Multiplicationmain.java
@@ -0,0 +1,15 @@
+package com.tnsif.Demo.Assignment2;
+import java.util.Scanner;
+public class Multiplicationmain {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter a number");
+		int number=sc.nextInt();
+		Multiplicationtable mt=new Multiplicationtable();
+		mt.table(number);
+		
+
+	}
+
+}
