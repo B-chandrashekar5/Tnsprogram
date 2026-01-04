772ce0d81e31a4dfc32551417cbd5e99efa7825f
info <info@shiva>
2026-01-04 15:54:28 +0530

    wrap

---
+++ Sri indu/com/tnsif/Demo/Day7/Wrapperclass/Autoboxingmain.java
@@ -0,0 +1,15 @@
+package com.tnsif.Demo.Day7.Wrapperclass;
+
+public class Autoboxingmain {
+
+	public static void main(String[] args) {
+		Autoboxing ab=new Autoboxing();
+		System.out.println(ab.i);
+		System.out.println(ab.x);
+		System.out.println(ab.c);
+		System.out.println(ab.v);
+		
+
+	}
+
+}
